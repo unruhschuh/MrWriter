a79d8c8ea47c6de3748c568c90e26ced4e142d3d
Thomas Leitz <thomas.leitz@web.de>
2018-12-30 22:26:36 +0100

    update appveyor.yml

--- travis/script_osx.sh
+++ travis/script_osx.sh
@@ -5,5 +5,4 @@ set -x
 qmake
 make
 macdeployqt MrWriter.app -dmg
-curl --upload-file MrWriter.dmg https://transfer.sh/MrWriter.dmg
-echo ""
+curl --upload-file MrWriter.dmg https://transfer.sh/MrWriter.dmg && echo ""
