43e8143897e235effda4bb293edba37e386074e3
Thomas Leitz <thomas.leitz@web.de>
2021-02-14 20:26:55 +0100

    redo travis

---
+++ .travis/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+set -e
+
+g++ --version
+
+mkdir build
+cd build
+#qmake CONFIG+=release ..
+conan install ..
+cmake .. --DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+cp -R ../appdir .
+make INSTALL_ROOT=appdir install ; find appdir/
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
+
+find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
+echo `curl --upload-file MrWriter*.AppImage https://transfer.sh/MrWriter-git.$(git rev-parse --short HEAD)-x86_64.AppImage`
+# wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
+# bash upload.sh MrWriter*.AppImage*
+
+
+
