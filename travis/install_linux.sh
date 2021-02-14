af6a00cff2c1f28c19667dce04d46ef4e1d3700b
Thomas Leitz <thomas.leitz@web.de>
2021-02-14 19:23:34 +0100

    install conan unter linux/travis

--- travis/install_linux.sh
+++ travis/install_linux.sh
@@ -2,9 +2,12 @@
 
 set -x
 
-sudo add-apt-repository ppa:beineri/opt-qt593-trusty -y
-sudo apt-get update -qq
-sudo apt-get -y install qt59base
-source /opt/qt*/bin/qt*-env.sh
+#sudo add-apt-repository ppa:beineri/opt-qt593-trusty -y
+#sudo apt-get update -qq
+#sudo apt-get -y install qt59base
+#source /opt/qt*/bin/qt*-env.sh
 
 #sudo apt-get -y install qt5-default
+
+pip install conan
+conan user
