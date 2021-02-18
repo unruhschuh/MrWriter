2c0c853040d5fa6ea9958999eb67dd1adfb0096e
Thomas Leitz <thomas.leitz@web.de>
2021-02-18 22:00:08 +0100

    switched to osx_image: xcode11.3

--- travis/script_osx.sh
+++ travis/script_osx.sh
@@ -10,3 +10,4 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
 macdeployqt bin/MrWriter.app -dmg
 echo `curl --upload-file bin/MrWriter.dmg https://transfer.sh/MrWriter.dmg`
+curl --upload-file bin/MrWriter.dmg -s -w "\n" https://transfer.sh/MrWriter.dmg
