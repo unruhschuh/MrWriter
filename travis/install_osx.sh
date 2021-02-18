3b17312a8410ac49e09c18446defe1a8610e3ca9
Thomas Leitz <thomas.leitz@web.de>
2021-02-18 21:48:34 +0100

    added osx build to travis

--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -3,5 +3,6 @@
 set -x
 
 brew update
-brew install qt
-brew link --force qt
+brew install conan
+#brew install qt
+#brew link --force qt
--- travis/script_osx.sh
+++ travis/script_osx.sh
@@ -3,11 +3,10 @@
 set -x
 set -e
 
-#qmake CONFIG+=release
 mkdir build
 cd build
 conan install ..
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make ..
+cmake --build . --config Release
 macdeployqt bin/MrWriter.app -dmg
 echo `curl --upload-file bin/MrWriter.dmg https://transfer.sh/MrWriter.dmg`
