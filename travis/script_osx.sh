b62f7f160034a29d8869091c4cf440282ff6c97c
Thomas Leitz <thomas.leitz@web.de>
2021-02-19 22:40:34 +0100

    travis

--- travis/script_osx.sh
+++ travis/script_osx.sh
@@ -6,7 +6,7 @@ set -e
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 mkdir build
 cd build
-conan install .. -s compiler=apple-clang
+conan install .. -s compiler=gcc
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
 macdeployqt bin/MrWriter.app -dmg
