3e1f123aea9d5e81b24316be12c299ad2be34e23
Thomas Leitz <thomas.leitz@web.de>
2021-02-21 11:48:38 +0100

    switched from docker to native on linux build on travis

--- travis/install_linux.sh
+++ travis/install_linux.sh
@@ -2,14 +2,8 @@
 
 set -x
 
-#sudo add-apt-repository ppa:beineri/opt-qt593-trusty -y
-#sudo apt-get update -qq
-#sudo apt-get -y install qt59base
-#source /opt/qt*/bin/qt*-env.sh
+sudo add-apt-repository ppa:beineri/opt-qt-5.15.2-xenial -y
+sudo apt-get update -qq
+sudo apt-get -y install qt515base libgl1-mesa-dev
+source /opt/qt*/bin/qt*-env.sh
 
-#sudo apt-get -y install qt5-default
-
-sudo apt-get update
-sudo apt-get install python3-pip
-sudo pip3 install conan
-conan user
--- travis/script_linux.sh
+++ travis/script_linux.sh
@@ -2,28 +2,19 @@
 
 set -x
 
-g++ --version
-
-#source /opt/qt*/bin/qt*-env.sh
-
-set -e
 mkdir build
 cd build
-#qmake CONFIG+=release ..
-conan install ..
-cmake .. --DCMAKE_BUILD_TYPE=Release
-make -j$(nproc)
-cp -R ../appdir .
-make INSTALL_ROOT=appdir install ; find appdir/
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --config Release
+
+cp -r ../appdir .
+make install
+
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
-
-find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
-echo `curl --upload-file MrWriter*.AppImage https://transfer.sh/MrWriter-git.$(git rev-parse --short HEAD)-x86_64.AppImage`
-# wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
-# bash upload.sh MrWriter*.AppImage*
-
+./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
+PATH=$PATH:./bin ./squashfs-root/AppRun appdir/usr/share/applications/MrWriter.desktop -appimage
+APPIMAGE=`ls MrWriter*.AppImage`
+curl --upload-file "./${APPIMAGE}" -s -w "\n" http://transfer.sh/${APPIMAGE}
 
